3dfbf650d69404d1f71418d4d72b75f861bc55fc
prikhodkin <prikhodkin_ilya@mail.ru>
2018-09-21 17:15:48 +0500

    Сделал отправку формы

---
+++ js/moduls/backend.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  // var URL_GET = 'https://js.dump.academy/candyshop/data';
+  var URL_POST = 'https://js.dump.academy/candyshop';
+
+
+  var upload = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Ошибка: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    upload: upload,
+  };
+
+})();
